e60bae33a12e1ad6d4d1c5e74afb91c1e6e48ef4
harshitsinha9000 <harshitsinha9000@gmail.com>
2017-02-17 15:10:37 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,17 @@
+//assignment 3.3  ans d. The program prints pnt is null 0 0.
+package java_session3;
+
+public class Example {
+	
+	int x; int y;
+	String name;
+	public static void main(String args[]) 
+	{
+		Example pnt = new Example();
+		System.out .println ("pnt is " + pnt .name + " " + pnt .x + " " + pnt .y);
+	}
+
+}
+
+
+//output ans d. The program prints pnt is null 0 0.
